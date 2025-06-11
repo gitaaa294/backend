ee213bc5af7f5f750a781cbaf21a7304749d49d4
gitaaa294 <defamingo18@gmail.com>
2025-06-12 05:00:20 +0700

    db.js

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'shortline.proxy.rlwy.net',
-  port: 46067,
-  user: 'root',
-  password: 'KBYkFCSjFLbbcKxjZMJOVwEkdfdAKSrf',
-  database: 'railway'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-
-connection.connect(err => {
-  if (err) throw err;
-  console.log('Koneksi ke database berhasil!');
+db.connect((err) => {
+  if (err) {
+    console.error('Gagal konek ke database:', err);
+  } else {
+    console.log('Koneksi ke database sukses!');
+  }
 });
 
-module.exports = connection;
+module.exports = db;
