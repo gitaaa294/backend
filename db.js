98dd0988b8f9780f4e145333aa9a48cd2056dddf
gitaaa294 <defamingo18@gmail.com>
2025-06-12 04:52:26 +0700

    db.js

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+const db = mysql.createConnection({
+  host: 'shortline.proxy.rlwy.net',
+  port: 46067,
+  user: 'root',
+  password: 'KBYkFCSjFLbbcKxjZMJOVwEkdfdAKSrf',
+  database: 'railway'
 });
+
 
 connection.connect(err => {
   if (err) throw err;
